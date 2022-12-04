87aa4fd44eb79784d727ed00443c6e5fed83edf5
aaronsleepy <aaron@kmong.com>
2022-12-04 12:47:13 +0900

    feat: searchControl 설정

--- GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/View/SearchViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        embedSearchControl()
         
     }
     
     // searchController 구성 및 navigationItem에 추가
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "aaronsleepy"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        
+        self.navigationItem.searchController = searchController
+    }
     
     // collectionView 구성
     
@@ -25,5 +41,18 @@ class SearchViewController: UIViewController {
     //   - searchController 검색 -> 네트워크 요청
 
 
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
+    }
 }
 
