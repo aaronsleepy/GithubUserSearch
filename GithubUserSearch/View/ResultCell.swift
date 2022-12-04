95a48ff0b0e9c26453f1524298121a02f9d693ca
aaronsleepy <aaron@kmong.com>
2022-12-04 12:18:17 +0900

    first commit

---
+++ GithubUserSearch/View/ResultCell.swift
@@ -0,0 +1,14 @@
+//
+//  ResultCell.swift
+//  GithubUserSearch
+//
+//  Created by Aaron on 2022/12/04.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    
+    @IBOutlet weak var user: UILabel!
+    
+}
