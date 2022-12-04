985d6873259038ac7e478899ce1ab2d8e8490b18
aaronsleepy <aaron@kmong.com>
2022-12-04 21:18:33 +0900

    feat: 검색 관련 Model 추가

---
+++ GithubUserSearch/Model/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResult.swift
+//  GithubUserSearch
+//
+//  Created by Aaron on 2022/12/04.
+//
+
+import Foundation
---
+++ GithubUserSearch/Model/SearchUserResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SearchUserResponse.swift
+//  GithubUserSearch
+//
+//  Created by Aaron on 2022/12/04.
+//
+
+import Foundation
